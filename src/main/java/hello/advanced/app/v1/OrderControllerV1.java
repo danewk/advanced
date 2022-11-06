c57ff68ab3bdc115b7de40a2bd88751270b601a1
kdanwoo <kdanwoo@jobis.co>
2022-11-06 20:21:08 +0900

    로그 추적기 V2 - 파라미터로 동기화 개발

--- src/main/java/hello/advanced/app/v1/OrderControllerV1.java
+++ src/main/java/hello/advanced/app/v1/OrderControllerV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderControllerV1 {
 
   private final OrderServiceV1 orderService;
+  private final HelloTraceV1 traceV1;
 
-  @GetMapping("/v0/request")
+  @GetMapping("/v1/request")
   public String request(String itemId) {
-    orderService.orderItem(itemId);
-    return "ok";
+
+    TraceStatus traceStatus = null;
+
+    try {
+      traceStatus = traceV1.begin("OrderController.request()");
+      orderService.orderItem(itemId);
+      traceV1.end(traceStatus);
+      return "ok";
+    } catch (Exception e) {
+      traceV1.exception(traceStatus, e);
+      throw e;
+    }
+
   }
 
 
--- src/main/java/hello/advanced/app/v1/OrderServiceV1.java
+++ src/main/java/hello/advanced/app/v1/OrderServiceV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +10,19 @@ import org.springframework.stereotype.Service;
 public class OrderServiceV1 {
 
   private final OrderRepositoryV1 orderRepository;
+  private final HelloTraceV1 traceV1;
 
   public void orderItem(String itemId) {
-    orderRepository.save(itemId);
+    TraceStatus status = null;
+
+    try {
+      status = traceV1.begin("OrderService.orderItem()");
+      orderRepository.save(itemId);
+      traceV1.end(status);
+    } catch (Exception e) {
+      traceV1.exception(status, e);
+      throw e;
+    }
   }
 
 }
